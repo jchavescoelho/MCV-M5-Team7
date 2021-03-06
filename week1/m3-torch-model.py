5a4bf98fa32b7bb746b552861c1ef6915c81d941
jchavescoelho <jchavescoelho@gmail.com>
2021-03-07 00:26:23 +0100

    Working m3 model in torch with train, validation and testing accuracy

--- week1/m3-torch-model.py
+++ week1/m3-torch-model.py
@@ -14,17 +14,17 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         #m3-layers
-        self.conv0 = nn.Conv2d(3, 64, 3) #dim in, dim out, size ker
+        self.conv0 = nn.Conv2d(3, 64, 3, padding=1) #dim in, dim out, size ker
         self.bn0 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
         self.pool0 = nn.MaxPool2d(2, 2)
         self.drop0 = nn.Dropout2d(0.2)
 
         #block1
-        self.conv1_1 = nn.Conv2d(64, 64, 3) #dim in, dim out, size ker
+        self.conv1_1 = nn.Conv2d(64, 64, 3, padding=1) #dim in, dim out, size ker
         self.bn1_1 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
         self.drop1_1 = nn.Dropout2d(0.2)
 
-        self.conv1_2 = nn.Conv2d(64, 64, 3)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn1_2 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
         self.drop1_2 = nn.Dropout2d(0.2)
 
@@ -32,11 +32,11 @@ class Net(nn.Module):
         self.drop1_p = nn.Dropout2d(0.2)
 
         #block2
-        self.conv2_1 = nn.Conv2d(64, 64, 3) #dim in, dim out, size ker
+        self.conv2_1 = nn.Conv2d(64, 64, 3, padding=1) #dim in, dim out, size ker
         self.bn2_1 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
         self.drop2_1 = nn.Dropout2d(0.2)
 
-        self.conv2_2 = nn.Conv2d(64, 64, 3)
+        self.conv2_2 = nn.Conv2d(64, 64, 3, padding=1)
         self.bn2_2 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
         self.drop2_2 = nn.Dropout2d(0.2)
 
@@ -71,7 +71,7 @@ class Net(nn.Module):
 
         return x
 
-
+MODEL_PATH = './m3-last.pth'
 BATCH_SIZE = 32
 LEARNING_RATE = 1e-5
 EPOCHS = 10
@@ -95,13 +95,14 @@ def main():
     DATASET_DIR = '~/code/uab_cv_master/m3/Databases/MIT_split'
 
     transfom_list = [tv.transforms.ToTensor(), tv.transforms.Resize((64, 64))]
+
     train_val_dataset = tv.datasets.ImageFolder(os.path.join(DATASET_DIR, 'train'), transform=tv.transforms.Compose(transfom_list))
     train_dataset, val_dataset = torch.utils.data.random_split(train_val_dataset, [4*len(train_val_dataset)//5, len(train_val_dataset) - 4*len(train_val_dataset)//5])
     test_dataset = tv.datasets.ImageFolder(os.path.join(DATASET_DIR, 'test'), transform=tv.transforms.Compose(transfom_list))
 
     train_generator = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, num_workers=4)
     val_generator = torch.utils.data.DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, num_workers=4)
-    test_generator = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, drop_last=True, num_workers=4)
+    test_generator = torch.utils.data.DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=False, num_workers=4)
 
     # # Visualize dataset
     # train_it = iter(train_generator)
@@ -119,49 +120,76 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    # Loss
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+    if not os.path.exists(MODEL_PATH):
 
-    t0 = time.time()
-    # Train
-    for epoch in range(EPOCHS):  # loop over the dataset multiple times
+        # Loss
+        criterion = nn.CrossEntropyLoss()
+        # optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+        optimizer = optim.Adam(model.parameters(), eps=1e-7)
 
-        train_correct = 0
-        val_correct = 0
-        for i, data in enumerate(train_generator, 0):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs, labels = data[0].to(device), data[1].to(device)
+        t0 = time.time()
+        # Train
+        for epoch in range(EPOCHS):  # loop over the dataset multiple times
 
-            # zero the parameter gradients
-            optimizer.zero_grad()
-
-            # forward + backward + optimize
-            outputs = model(inputs) # forward pass
-            loss = criterion(outputs, labels) # calculate loss
-            loss.backward() # backpropagate loss
-            optimizer.step() # update weights/parameters
-
-            # Compute training accuracy
-            train_correct += (torch.max(outputs, dim=1).indices == labels).float().sum()
-
-        # Compute val accuracy
-        with torch.no_grad():
-            for i, data in enumerate(val_generator, 0):
+            train_correct = 0
+            val_correct = 0
+            for i, data in enumerate(train_generator, 0):
+                # get the inputs; data is a list of [inputs, labels]
                 inputs, labels = data[0].to(device), data[1].to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
                 outputs = model(inputs) # forward pass
+                loss = criterion(outputs, labels) # calculate loss
+                loss.backward() # backpropagate loss
+                optimizer.step() # update weights/parameters
 
-                val_correct += (torch.max(outputs, dim=1).indices == labels).float().sum()
+                # Compute training accuracy
+                train_correct += (torch.max(outputs, dim=1).indices == labels).float().sum()
+
+            # Compute val accuracy
+            with torch.no_grad():
+                for i, data in enumerate(val_generator, 0):
+                    inputs, labels = data[0].to(device), data[1].to(device)
+                    outputs = model(inputs) # forward pass
+
+                    val_correct += (torch.max(outputs, dim=1).indices == labels).float().sum()
 
 
-        accuracy = 100 * train_correct / len(train_dataset)
-        val_accuracy = 100 * val_correct / len(val_dataset)
-        
-        # print statistics
-        print(f'Epoch {epoch}. Training accuracy = {accuracy}. Validation accuracy = {val_accuracy}')
+            accuracy = 100 * train_correct / len(train_dataset)
+            val_accuracy = 100 * val_correct / len(val_dataset)
 
-    print(f'Finished Training. Device: {device}. Elapsed time {round(time.time() - t0, 2)} s')
+            # print statistics
+            print(f'Epoch {epoch}. Training accuracy = {accuracy}. Validation accuracy = {val_accuracy}')
 
+        print(f'Finished Training. Device: {device}. Elapsed time {round(time.time() - t0, 2)} s')
+        # Save model
+        torch.save(model.state_dict(), MODEL_PATH)
+    else:
+        model.load_state_dict(torch.load(MODEL_PATH)) # this is how you load a saved model
+
+    # test
+    dataiter = iter(test_generator)
+    ims, labels = dataiter.next()
+
+    idx = 17
+    imshow(np.moveaxis(ims[15].numpy(), 0, -1)) # imshow(tv.transforms.ToPILImage()(ims[0]))
+    print(CLASSES[labels[15].item()])
+    plt.show()
+
+
+    # Test
+    test_correct = 0
+    with torch.no_grad():
+        for data in test_generator:
+            inputs, labels = data[0].to(device), data[1].to(device)
+            outputs = model(inputs)
+            test_correct += (torch.max(outputs, dim=1).indices == labels).float().sum()
+
+    test_accuracy = 100 * test_correct / len(test_dataset)
+    print(f'Accuracy on test: {test_accuracy}')
 
 
 if __name__ == '__main__':
