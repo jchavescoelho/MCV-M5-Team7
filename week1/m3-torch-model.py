99fc676a774f10b3b82634fc3f15392784881d69
Carmen GarciaNo <carmengnogu@gmail.com>
2021-03-06 17:59:07 +0100

    initial structure m3-net

--- week1/m3-torch-model.py
+++ week1/m3-torch-model.py
@@ -10,22 +10,56 @@ import torchvision.transforms as transforms
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        # input dime 3 channel image
-        # output dim 6 num filters
-        # size filter 5x5
-        # self.conv1 = nn.Conv2d(3, 18, 5) 
-        # self.pool = nn.MaxPool2d(2, 2)
-        # self.conv2 = nn.Conv2d(18, 16, 5)
-        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        # self.fc2 = nn.Linear(120, 84)
-        # self.fc3 = nn.Linear(84, 10)
+        #m3-layers
+        self.conv0 = nn.Conv2D(3, 64, 3) #dim in, dim out, size ker
+        self.bn0 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
+        self.pool0 = nn.MaxPool2d(2, 2)
+        self.drop0 = nn.Dropout2d(0.2)
+
+        #block1
+        self.conv1_1 = nn.Conv2D(3, 64, 3) #dim in, dim out, size ker
+        self.bn1_1 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
+        self.drop1_1 = nn.Dropout2d(0.2)
+
+        self.conv1_2 = nn.Conv2D(64, 64, 3)
+        self.bn1_2 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
+        self.drop1_2 = nn.Dropout2d(0.2)
+
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.drop1_p = nn.Dropout2d(0.2)
+
+        #block2
+        self.conv2_1 = nn.Conv2D(3, 64, 3) #dim in, dim out, size ker
+        self.bn2_1 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
+        self.drop2_1 = nn.Dropout2d(0.2)
+
+        self.conv2_2 = nn.Conv2D(64, 64, 3)
+        self.bn2_2 = nn.BatchNorm2d(64, eps=1e-03, momentum=0.99)
+        self.drop2_2 = nn.Dropout2d(0.2)
+
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.drop2_p = nn.Dropout2d(0.2)
+
+        self.avgPool = nn.AvgPool2d()
+        self.fc = nn.Linear(64, 8)
+
         pass
 
     def forward(self, x):
-        # x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        # x = x.view(-1, 16 * 5 * 5)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
+        x = self.drop0(self.pool0(self.bn0(F.relu(self.conv0(x))))) #layer 0
+
+        #block1
+        x = self.drop1_1(self.bn1_1(F.relu(self.conv1_1(x))))
+        x = self.drop1_2(self.bn1_2(F.relu(self.conv1_2(x))))
+        x = self.drop1_p(self.pool1(x))
+
+        #block2
+        x = self.drop2_1(self.bn2_1(F.relu(self.conv2_1(x))))
+        x = self.drop2_2(self.bn2_2(F.relu(self.conv2_2(x))))
+        x = self.drop2_p(self.pool2(x))
+
+        x = F.softmax(self.fc(self.avgPool(x)))
+
         return x
+
+#TODO:RESIDUAL
