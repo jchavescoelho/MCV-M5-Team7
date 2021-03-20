56934941106855cd5d41eff72ace72ed38426178
jchavescoelho <jchavescoelho@gmail.com>
2021-03-20 21:14:03 +0100

    Trying to filter only cars and people (fixed dumb bug)

--- week3/evaluate.py
+++ week3/evaluate.py
@@ -92,7 +92,7 @@ for d in random.sample(mots_train_dicts, 5):
     file_name = d['file_name']
     im = cv2.imread(file_name)
     outputs = predictor(im)
-    # inst = outputs["instances"]
+    inst = outputs["instances"]
     inst = isnt[inst.pred_classes == 0 or inst.pred_classes == 2]
     # instances = outputs["instances"][outputs["instances"].scores > 0.5]
     v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1)
