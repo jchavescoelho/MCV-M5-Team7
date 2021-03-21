7f5a8ba80da7154c43fd41362440693e9f604d13
Carmen GarciaNo <carmengnogu@gmail.com>
2021-03-21 21:36:27 +0100

    Added configs

--- week3/train.py
+++ week3/train.py
@@ -19,29 +19,86 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 
 from detectron2.engine import DefaultTrainer
 
+OUTPUT_DIR = './experiments'
+
 #Fine tuning config
 learn_rates = [0.00025, 0.0005, 0.001, 0.01, 0.1, 1]
 models = ["COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml", "COCO-Detection/retinanet_R_50_FPN_3x.yaml"]
-datasets = ['mots_train', 'kitty_train']
-BATCH_SIZE_PER_IMAGE = [32, 64, 128, 254, 512]
+datasets = ['mots_train', 'kitti-mots_train']
+batchs = [32, 64, 128, 254, 512]
 
-experiment_name = f'{ds}_{model[15:-5]}_lr{lr}_batch{batch}'
+# Load/Register datasets
+ds_name = 'kitti-mots'
+kittimots_train_dicts, kittimots_val_dicts = ds.get_mots_dicts(KITTI_MOTS_PATH, ds_name)
 
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("balloon_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+labels = set()
+# remap dataset class labels
+for dataset in [kittimots_train_dicts, kittimots_val_dicts]:
+    for image in dataset:
+        for obj in image['annotations']:
+            if obj['category_id'] == 1:
+                obj['category_id'] = 2
+            elif obj['category_id'] == 2:
+                obj['category_id'] = 0
+            labels.add(obj['category_id'])
+print('LABELS', labels)
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=False)
-trainer.train()
+print('Registering...')
+DatasetCatalog.register(ds_name+'_train', lambda : kittimots_train_dicts)
+MetadataCatalog.get(ds_name+'_train').set(thing_classes=['pedestrian', 'bike', 'car'])
+
+DatasetCatalog.register(ds_name+'_val', lambda : kittimots_val_dicts)
+MetadataCatalog.get(ds_name+'_val').set(thing_classes=['pedestrian', 'bike', 'car'])
+
+ds_name = 'mots'
+mots_train_dicts, mots_val_dicts = ds.get_mots_dicts(MOTS_PATH, ds_name)
+
+labels = set()
+# remap dataset class labels
+for dataset in [mots_train_dicts, mots_val_dicts]:
+    for image in dataset:
+        for obj in image['annotations']:
+            if obj['category_id'] == 1:
+                obj['category_id'] = 2
+            elif obj['category_id'] == 2:
+                obj['category_id'] = 0
+            labels.add(obj['category_id'])
+print('LABELS', labels)
+
+allmots_train_dicts = {**kittimots_train_dicts, **mots_train_dicts}
+allmots_val_dicts = {**kittimots_val_dicts, **mots_val_dicts}
+
+print('Registering...')
+DatasetCatalog.register(ds_name+'_train', lambda : allmots_train_dicts)
+MetadataCatalog.get(ds_name+'_train').set(thing_classes=['pedestrian', 'bike', 'car'])
+
+DatasetCatalog.register(ds_name+'_val', lambda : allmots_val_dicts)
+MetadataCatalog.get(ds_name+'_val').set(thing_classes=['pedestrian', 'bike', 'car'])
+
+for lr in learn_rates:
+    for model in models:
+        for ds in datasets:
+            for batch in batchs:
+
+                experiment_name = f'{ds}_{model[15:-5]}_lr{lr}_batch{batch}'
+
+                cfg = get_cfg()
+                cfg.merge_from_file(model_zoo.get_config_file(model))
+                cfg.DATASETS.TRAIN = (ds,)
+                cfg.DATASETS.TEST = ()
+                cfg.DATALOADER.NUM_WORKERS = 4
+                cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model)  # Let training initialize from model zoo
+                cfg.SOLVER.IMS_PER_BATCH = 2
+                cfg.SOLVER.BASE_LR = lr      # pick a good LR
+                cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+                cfg.SOLVER.STEPS = []        # do not decay learning rate
+                cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = batch   # faster, and good enough for this toy dataset (default: 512)
+                cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+                # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+                cfg.OUTPUT_DIR = os.path.join(OUTPUT_DIR, experiment_name)
+                os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+
+                trainer = DefaultTrainer(cfg) 
+                trainer.resume_or_load(resume=False)
+                trainer.train()
