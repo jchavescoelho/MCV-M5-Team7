ba9d6dc62847b78aa5cbd493f64c7ee76bc23a8b
jchavescoelho <jchavescoelho@gmail.com>
2021-03-21 22:08:25 +0100

    wip

--- week3/evaluate.py
+++ week3/evaluate.py
@@ -78,25 +78,26 @@ for d in random.sample(mots_train_dicts, 5):
 
 # Pre-trained
 print('Loading pre-trained models...')
-cfg = get_cfg()
 
 #Select model
 classes = ('pedestrian', 'car')
 model_zoo_yml = "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"
+model_zoo_yml = "COCO-Detection/retinanet_R_50_FPN_3x.yaml"
+folder_name = model_zoo_yml.split('/')[-1].split('.')[0]
+
+cfg = get_cfg()
+cfg.OUTPUT_DIR = os.path.join('models', folder_name)
+
 # cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_50_FPN_3x.yaml"))
 cfg.merge_from_file(model_zoo.get_config_file(model_zoo_yml))
-
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
 
 # cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/retinanet_R_50_FPN_3x.yaml")
 cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_zoo_yml)
 
-folder_name = model_zoo_yml.split('/')[-1].split('.')[0]
 
 # Training goes here
 if TRAIN:
     from detectron2.engine import DefaultTrainer
-    cfg.OUTPUT_DIR = os.path.join('models', folder_name)
     print('Configuring training...')
     cfg.DATASETS.TRAIN = (ds_name + "_train",)
     cfg.DATASETS.TEST = ()
@@ -119,6 +120,8 @@ if TRAIN:
     cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
 
 # Random inferences
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+
 predictor = DefaultPredictor(cfg)
 os.makedirs(f'./sampleinfer/{folder_name}', exist_ok=True)
 
